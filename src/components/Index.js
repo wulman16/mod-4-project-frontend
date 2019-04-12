9945a25c6d1ef173371efe8fd67856230904a92a
wulman16 <will.ulman@gmail.com>
2019-04-12 11:10:24 -0400

    render basic routes

---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Index!</h2>
+      </div>
+    )
+  }
+}
+
+export default Index
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './components/Login.js'
+import Index from './components/Index.js'
+import Profile from './components/Profile.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Title = () => 
+  <h1>Clothing Game</h1>
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Title />
+      <Route exact path="/login" render={Login} />
+      <Route exact path="/index" render={Index} />
+      <Route exact path="/profile" render={Profile} />
+    </React.Fragment>
+  </Router>),
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
